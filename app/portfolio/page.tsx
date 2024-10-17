7144a56c3a97e20b74e45e582ca3d5fe8897c2bc
Lawrence Jefferson II <jefftkddan@gmail.com>
2024-10-17 15:02:46 -0700

    updated portfolio page

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -19,48 +19,25 @@ type Project = {
 const projects: Project[] = [
   {
     id: 1,
-    title: "Vast AI Gang Dashboard Application",
-    img: "/mockups/vastaigang.png",
+    title: "1Kloc-Useful Apps in 1,000 lines of Code",
+    img: "/mockups/working_on_1kloc.png",
     description:
-      "As part of an Agile team working with Vast AI Gang LLC and V School, I contributed to developing a dashboard application for hosts renting out their machines on Vast AI. My responsibilities included creating features, refactoring code, and ensuring seamless integration between the frontend and backend. The application provided a user-friendly interface for monitoring machine metrics, configuring notification settings, and generating reports. The project is a private repository of Vast AI Gang LLC.",
+      " I am creating and will be principle maintainer of the open source project 1Kloc. Welcome to the 1Kloc project! This is an open-source initiative to build useful apps under 1,000 lines of code. Explore, contribute, and showcase your coding skills! We are Under Construction and working on our Docs to get things going, so the docs link below is to our local dev server right now. Hoepfully we will be deployed for tests and showcase within the week!!!",
     stacks: [
       "HTML",
       "Tailwind CSS",
-      "React.js",
       "JavaScript",
-      "TypeScript",
-      "Redux",
-      "NxDev Monorepo",
-      "Nest.js",
+      "React",
+      "React-Router-Dom",
+      "Docusaurus",
       "Firebase",
-      "Chart.js",
-      "React-chartjs-2",
-      "Axios",
-      "Tailwind UI",
+      "Vite",
     ],
     live: "",
-    gitHub: "",
+    gitHub: "https://github.com/1kloc-Useful-Apps-in-1-000-Lines",
   },
   {
     id: 2,
-    title: "Python & React App",
-    img: "/mockups/python-scfi.png",
-    description:
-      "The backend is built with FastAPI and MongoDB Atlas, while the frontend uses React, Vite, TypeScript, and Tailwind CSS. Welcome to the Sci-Fi Themed CRUD Application! This app lets you manage characters in a sci-fi universe. You can create, read, update, and delete characters. Note: Sorry !!! This takes about two minutes to spin up on free tier of render deployment.",
-    stacks: [
-      "HTML",
-      "Tailwind CSS",
-      "React.js",
-      "TypeScript",
-      "MongoDB Atlas",
-      "Python FastAPI",
-      "Vite",
-    ],
-    live: "https://sci-fi-app-frontend.onrender.com",
-    gitHub: "https://github.com/MenokoOG/sci-fi-app",
-  },
-  {
-    id: 3,
     title: "LuxGirl OG - Original Geek! Website",
     img: "/mockups/Screenshot 2024-06-01 132640.png",
     description:
@@ -70,7 +47,7 @@ const projects: Project[] = [
     gitHub: "https://github.com/MenokoOG/luxgirl-portfolio",
   },
   {
-    id: 4,
+    id: 3,
     title: "Grocery List App",
     img: "/mockups/grocery.png",
     description:
@@ -88,11 +65,11 @@ const projects: Project[] = [
     gitHub: "https://github.com/MenokoOG/luxgirl-grocery-app",
   },
   {
-    id: 5,
+    id: 4,
     title: "MERN Stack Helper 2.0",
     img: "/mockups/Screenshot 2024-03-27 163015.png",
     description:
-      "This application is a full stack application with authentication. It uses HTML, CSS, JavaScript, MongoDB, Express.js, React + Vite, and Node.js. This application demonstrates complex routing, multiple theme options, and user-specific CRUD operations with custom API. MERN Stack Helper, our primary aim is to support fellow students in their journey through the Full Stack Web Development course.",
+      "This application is a full stack application with authentication. It uses HTML, CSS, JavaScript, MongoDB, Express.js, React + Vite, and Node.js. My goal was to create a simple and easy-to-use application that helps developers quickly set up a MERN stack project with authentication. The application uses JWT for authentication and Bcrypt for password hashing.",
     stacks: [
       "Node.js",
       "Express.js",
@@ -110,32 +87,13 @@ const projects: Project[] = [
     live: "https://mern-stack-helper-2-0.onrender.com",
     gitHub: "https://github.com/MenokoOG/mern_stack_help_2",
   },
+
   {
-    id: 6,
-    title: "FizzBuzz React",
-    img: "/mockups/fizzBuzz.png",
-    description:
-      "This is a fun and interactive FizzBuzz game built with React and Tailwind CSS. The application allows users to select a range using a slider or input a number directly to generate the FizzBuzz sequence. The cards representing the sequence are styled with cool effects and animations to make the experience more engaging. I am in process of adding more features to this project and making it mobile first.",
-    stacks: ["React.js", "Tailwind CSS", "JavaScript", "Vite"],
-    live: "https://fizzbuzzreact.netlify.app/",
-    gitHub: "https://github.com/MenokoOG/fizzbuzz-react",
-  },
-  {
-    id: 7,
-    title: "JavaScript Back-end and Python Front-end",
-    img: "/mockups/random_dog.png",
-    description:
-      "This is a simple project that combines a JavaScript backend using Express and a Python frontend using Flask. It fetches random dog images from the Dog CEO API and displays them with a nice Tailwind CSS style.",
-    stacks: ["Node.js", "Express.js", "CSS", "HTML", "Python Flask"],
-    live: "",
-    gitHub: "https://github.com/MenokoOG/js-py",
-  },
-  {
-    id: 8,
+    id: 5,
     title: "My 2010-2013 Web Projects",
     img: "/mockups/Screenshot 2024-03-17 115744.png",
     description:
-      "Web projects I developed between 2010 and 2013. The projects are a mix of HTML, CSS, JavaScript, and Adobe Master Collections. They include a personal portfolio, my old military blog site, a news site, a couple one page landing sites, and a website for a motor cycle club. The projects are fully responsive and interactive.",
+      "Web projects I developed between 2010 and 2013. The projects are a mix of HTML, CSS, JavaScript, and Adobe Master Collections. The projects were developed in Adobe Dreamweaver, Photoshop, and Illustrator. This was pre-React and pre-Node.js era so sites are not pretty or mobile first at all.They include a personal portfolio, my old military blog site, a news site, a couple one page landing sites, and a website for a motor cycle club. ",
     stacks: [
       "HTML",
       "CSS",
@@ -149,7 +107,7 @@ const projects: Project[] = [
     gitHub: "https://github.com/MenokoOG/Past-Web-Projects-for-Clients",
   },
   {
-    id: 9,
+    id: 6,
     title: "ToDo App in Nx Dev Monorepo",
     img: "/mockups/nx_todo.png",
     description:
@@ -161,6 +119,7 @@ const projects: Project[] = [
       "Typescript",
       "Nest.js",
       "MongoDB",
+      "Nx Dev",
     ],
     live: "",
     gitHub: "https://github.com/MenokoOG/todo-app",
@@ -171,8 +130,16 @@ const Portfolio = () => {
   return (
     <div className="container mx-auto py-10">
       <h1 className="text-4xl font-bold text-center mb-10 text-blue-600 dark:text-white">
-        Featured Works
+        Featured Works{" "}
       </h1>
+      <p className="text-sm">
+        I am currently in process of revamping this page with more organization
+        for my client projects, projects I have collaborated on, helped on, and
+        then my mess around projects. I have a couple of projects here though
+        for you to explore.
+      </p>
+      <br />
+
       <div className="flex flex-wrap justify-center">
         {projects.map((project) => (
           <div key={project.id} className="w-full sm:w-1/2 md:w-1/3 px-4 mb-8">
